49b93ffb83c03bc9b0b289fa5ceb909d4744a743
rgap <r.guzmanap@gmail.com>
2018-08-08 13:36:00 -0500

    quick update

--- Predict Future Sales/r.sh
+++ Predict Future Sales/r.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -N ttt1
-#PBS -e /Users/rgap/Desktop/kaggle_final/Predict Future Sales/pbs_out/log.txt
-#PBS -o /Users/rgap/Desktop/kaggle_final/Predict Future Sales/pbs_out/outlog.txt
+#PBS -e "/home/rguzman/Competitive-Data-Science-Models/Predict Future Sales/log.txt"
+#PBS -o "/home/rguzman/Competitive-Data-Science-Models/Predict Future Sales/outlog.txt"
 #PBS -l nodes=1:ppn=12
 cd "$PBS_O_WORKDIR"
 
