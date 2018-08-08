428a52b71340a5a115342fafdd5ed63b7ff4a9cb
rgap <r.guzmanap@gmail.com>
2018-08-08 14:03:22 -0500

    quick update

--- Predict_Future_Sales/r.sh
+++ Predict_Future_Sales/r.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #PBS -N ttt1
-#PBS -e /home/rguzman/Competitive-Data-Science-Models/Predict Future Sales/pbs_out/log.txt
-#PBS -o /home/rguzman/Competitive-Data-Science-Models/Predict Future Sales/pbs_out/outlog.txt
+#PBS -e /home/rguzman/Competitive-Data-Science-Models/Predict_Future_Sales/pbs_out/log.txt
+#PBS -o /home/rguzman/Competitive-Data-Science-Models/Predict_Future_Sales/pbs_out/outlog.txt
 #PBS -l nodes=1:ppn=12
 cd $PBS_O_WORKDIR
 
-/home/rguzman/venv/f/bin/python3 "/home/rguzman/Competitive-Data-Science-Models/Predict Future Sales/Simple model 6 - Hyperparameter Tuning.py"
+/home/rguzman/venv/f/bin/python3 "/home/rguzman/Competitive-Data-Science-Models/Predict_Future_Sales/Model_5_Hyperparameter_Tuning.py"
 
